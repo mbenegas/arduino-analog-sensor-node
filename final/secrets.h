86ba8b4df13731ae1e1b975e29549c49e4d36b28
Manuel Benegas <memebenegas@gmail.com>
2019-11-07 23:10:53 -0600

    Valores secretos

---
+++ final/secrets.h
@@ -0,0 +1,7 @@
+// WPA/WPA2
+#define SECRET_SSID ""    // reemplazar ssid
+#define SECRET_PASS ""   // reemplazar password wifi
+
+//ThingSpeak
+#define SECRET_CH_ID 0000000                     // reemplazar 0000000 con el numero de canal
+#define SECRET_WRITE_APIKEY "XYZ"  // reemplazar XYZ con el API Key
